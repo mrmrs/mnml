bb08ba6e0fc48f01de5ebc077ff64ae835e313ea
mrmrs <hi@mrmrs.cc>
2014-02-02 00:11:21 -0800

    Adds gulp tasks.

---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+
+// Load plugins
+
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    watch = require('gulp-watch'),
+    lr    = require('tiny-lr'),
+    server = lr(),
+    livereload = require('gulp-livereload'),
+    prefix = require('gulp-autoprefixer'),
+    minifyCSS = require('gulp-minify-css'),
+    sass = require('gulp-ruby-sass'),
+    csslint = require('gulp-csslint');
+
+
+// Task to minify all css files in the css directory
+
+gulp.task('minify-css', function(){
+  gulp.src('./css/*.css')
+    .pipe(minifyCSS({keepSpecialComments: 0}))
+    .pipe(gulp.dest('./css/'));
+});
+
+
+// Use csslint without box-sizing or compatible vendor prefixes (these
+// don't seem to be kept up to date on what to yell about)
+
+gulp.task('csslint', function(){
+  gulp.src('./css/*.css')
+    .pipe(csslint({
+          'compatible-vendor-prefixes': false,
+          'box-sizing': false,
+          'important': false,
+          'known-properties': false
+        }))
+    .pipe(csslint.reporter());
+
+});
+
+
+// Task that compiles scss files down to good old css
+
+gulp.task('pre-process', function(){
+  gulp.src('./sass/i.scss')
+      .pipe(watch(function(files) {
+        return files.pipe(sass({loadPath: ['./sass/'], style: "compact"}))
+          .pipe(prefix())
+          .pipe(gulp.dest('./css/'))
+          .pipe(livereload(server));
+      }));
+});
+
+gulp.task('reload-html', function() {
+  gulp.src('*.html')
+    .pipe(watch(function(files) {
+      return files.pipe(livereload(server));
+    }));
+});
+
+/*
+   DEFAULT TASK
+
+ • Process sass and lints outputted css
+ • Outputted css is run through autoprefixer
+ • Sends updates to any files in directory to browser for
+ automatic reloading
+
+*/
+
+gulp.task('default', function(){
+  gulp.run('pre-process', 'csslint');
+  server.listen(35729, function (err) {
+    gulp.watch(['*.html', './sass/*.scss'], function(event) {
+      gulp.run('reload-html','pre-process', 'csslint');
+    });
+  });
+});
+
