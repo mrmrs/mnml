4382699fb16d28446f37f5c7e8d2a3e5eedbecc2
mrmrs <hi@mrmrs.cc>
2015-09-27 19:10:28 +0300

    Port mnml from sass to postcss build process.
    * Add compiled src.
    * Rename partials
    * Strip out existing styles in mnml

---
+++ index.js
@@ -0,0 +1,34 @@
+// dependencies for mnml
+
+var fs = require("fs")
+var autoprefixer = require("autoprefixer")
+var postcss = require("postcss")
+var atImport = require("postcss-import")
+var cssvariables = require('postcss-css-variables')
+var compressor = require('node-minify')
+var conditionals = require('postcss-conditionals')
+var customMedia = require("postcss-custom-media")
+
+// css to be processed
+var css = fs.readFileSync("src/mnml.css", "utf8")
+
+// process css
+var output = postcss([autoprefixer])
+  .use(atImport())
+  .use(cssvariables())
+  .use(conditionals())
+  .use(customMedia())
+  .process(css, {
+    from: "./src/mnml.css",
+    to: "./css/mnml.css"
+  })
+  .css
+
+fs.writeFile("css/mnml.css", output, 'utf-8')
+
+// Using YUI Compressor for CSS
+new compressor.minify({
+    type: 'sqwish',
+    fileIn: './css/mnml.css',
+    fileOut: './css/mnml.min.css'
+});
