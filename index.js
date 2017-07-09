b63363df1e848c7e9f8fde3b3125246d87bfd7a5
Matt Downing <mattcdowning@gmail.com>
2017-07-09 10:34:40 -0400

    add semis

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // dependencies for mnml
 
-var fs = require("fs")
-var autoprefixer = require("autoprefixer")
-var postcss = require("postcss")
-var atImport = require("postcss-import")
-var cssvariables = require("postcss-css-variables")
-var compressor = require("node-minify")
-var conditionals = require("postcss-conditionals")
-var customMedia = require("postcss-custom-media")
+var fs = require("fs");
+var autoprefixer = require("autoprefixer");
+var postcss = require("postcss");
+var atImport = require("postcss-import");
+var cssvariables = require("postcss-css-variables");
+var compressor = require("node-minify");
+var conditionals = require("postcss-conditionals");
+var customMedia = require("postcss-custom-media");
 
 // css to be processed
-var css = fs.readFileSync("src/mnml.css", "utf8")
+var css = fs.readFileSync("src/mnml.css", "utf8");
 
 // process css
 output = postcss()
@@ -24,12 +24,12 @@ output = postcss()
     to: "./css/mnml.css"
   })
   .then(function(output) {
-    fs.writeFileSync("css/mnml.css", output, "utf-8")
-  })
+    fs.writeFileSync("css/mnml.css", output, "utf-8");
+  });
 
 // Using Clean-css for CSS
 compressor.minify({
   compressor: "clean-css",
   input: "./css/mnml.css",
   output: "./css/mnml.min.css"
-})
+});
