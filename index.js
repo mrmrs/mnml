55fb569027f846f380d185b79a298b86674b08e4
Matt Downing <mattcdowning@gmail.com>
2017-07-09 10:28:32 -0400

    update index.js to fix dep warnings, add package-lock.json to conform with npm 5

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // dependencies for mnml
 
-var fs = require("fs")
-var autoprefixer = require("autoprefixer")
-var postcss = require("postcss")
-var atImport = require("postcss-import")
+var fs = require('fs')
+var autoprefixer = require('autoprefixer')
+var postcss = require('postcss')
+var atImport = require('postcss-import')
 var cssvariables = require('postcss-css-variables')
 var compressor = require('node-minify')
 var conditionals = require('postcss-conditionals')
-var customMedia = require("postcss-custom-media")
+var customMedia = require('postcss-custom-media')
 
 // css to be processed
-var css = fs.readFileSync("src/mnml.css", "utf8")
+var css = fs.readFileSync('src/mnml.css', 'utf8')
 
 // process css
 output = postcss()
@@ -20,16 +20,16 @@ output = postcss()
   .use(customMedia())
   .use(autoprefixer())
   .process(css, {
-    from: "./src/mnml.css",
-    to: "./css/mnml.css"
-  }).then(function(output) {
-    fs.writeFile("css/mnml.css", output, 'utf-8')
-  });
-
+    from: './src/mnml.css',
+    to: './css/mnml.css'
+  })
+  .then(function(output) {
+    fs.writeFileSync('css/mnml.css', output, 'utf-8')
+  })
 
 // Using Clean-css for CSS
-new compressor.minify({
-    type: 'clean-css',
-    fileIn: './css/mnml.css',
-    fileOut: './css/mnml.min.css'
-});
+compressor.minify({
+  compressor: 'clean-css',
+  input: './css/mnml.css',
+  output: './css/mnml.min.css'
+})
